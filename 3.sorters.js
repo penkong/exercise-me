599d59b6484650cbc816f1571db90704878ee044
mazdak <nazemi.works@gmail.com>
2019-04-29 20:58:31 -0700

    some trim

--- 3.sorters.js
+++ 3.sorters.js
@@ -11,195 +11,6 @@ function bubbleSort(arr) {
   }
   return arr;
 }
-
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let indexOfMin = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[indexOfMin]) {
-        indexOfMin = j;
-      }
-    }
-    if (indexOfMin !== i) {
-      let lesser = arr[indexOfMin];
-      arr[indexOfMin] = arr[i];
-      arr[i] = lesser;
-      //swap()
-    }
-  }
-  return arr;
-}
-
-function mergeSort(arr) {
-  if (arr.length === 1) {
-    return arr;
-  }
-  const center = Math.floor(arr.length / 2);
-  const left = arr.slice(0, center);
-  const right = arr.slice(center);
-
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-function merge(left, right) {
-  const results = [];
-
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      results.push(left.shift());
-    } else {
-      results.push(right.shift());
-    }
-  }
-
-  return [...results, ...left, ...right];
-}
-
-function insertionSort(arr) {
-  var currentVal;
-  for (var i = 1; i < arr.length; i++) {
-    currentVal = arr[i];
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-      arr[j + 1] = arr[j]
-    }
-    arr[j + 1] = currentVal;
-  }
-  return arr;
-}
-
-function insertionSort(arr) {
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[0]) {
-      //move current element to the first position
-      arr.unshift(arr.splice(i, 1)[0]);
-    } else if (arr[i] > arr[i - 1]) {
-      //leave current element where it is
-      continue;
-    } else {
-      //find where element should go
-      for (var j = 1; j < i; j++) {
-        if (arr[i] > arr[j - 1] && arr[i] < arr[j]) {
-          //move element
-          arr.splice(j, 0, arr.splice(i, 1)[0]);
-        }
-      }
-    }
-  }
-  return arr;
-}
-
-function quickSort(array, left, right) {
-  const len = array.length;
-  let pivot;
-  let partitionIndex;
-
-  if (left < right) {
-    pivot = right;
-    partitionIndex = partition(array, pivot, left, right);
-
-    //sort left and right
-    quickSort(array, left, partitionIndex - 1);
-    quickSort(array, partitionIndex + 1, right);
-  }
-  return array;
-}
-
-function partition(array, pivot, left, right) {
-  let pivotValue = array[pivot];
-  let partitionIndex = left;
-
-  for (let i = left; i < right; i++) {
-    if (array[i] < pivotValue) {
-      swap(array, i, partitionIndex);
-      partitionIndex++;
-    }
-  }
-  swap(array, right, partitionIndex);
-  return partitionIndex;
-}
-
-function swap(array, firstIndex, secondIndex) {
-  var temp = array[firstIndex];
-  array[firstIndex] = array[secondIndex];
-  array[secondIndex] = temp;
-}
-//Select first and last index as 2nd and 3rd parameters
-quickSort(numbers, 0, numbers.length - 1);
-console.log(numbers);
-// //#1 - Sort 10 schools around your house by distance:
-// insertion sort
-
-// //#2 - eBay sorts listings by the current Bid amount:
-// radix or counting sort
-
-// //#3 - Sort scores on ESPN
-// Quick sort
-
-// //#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
-// Merge Sort
-
-// //#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
-// Insertion Sort
-
-// //#6 - Temperature Records for the past 50 years in Canada
-// radix or counting Sort
-// Quick sort if decimal places
-
-// //#7 - Large user name database needs to be sorted. Data is very random.
-// Quick sort
-
-// //#8 - You want to teach sorting
-// Bubble sort
-//============================
-/binary search 
-// Original Solution
-function binarySearch(arr, elem) {
-  var start = 0;
-  var end = arr.length - 1;
-  var middle = Math.floor((start + end) / 2);
-  while (arr[middle] !== elem && start <= end) {
-    if (elem < arr[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
-    middle = Math.floor((start + end) / 2);
-  }
-  if (arr[middle] === elem) {
-    return middle;
-  }
-  return -1;
-}
-
-// Refactored Version
-function binarySearch(arr, elem) {
-  var start = 0;
-  var end = arr.length - 1;
-  var middle = Math.floor((start + end) / 2);
-  while (arr[middle] !== elem && start <= end) {
-    if (elem < arr[middle]) end = middle - 1;
-    else start = middle + 1;
-    middle = Math.floor((start + end) / 2);
-  }
-  return arr[middle] === elem ? middle : -1;
-}
-
-binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103)
-//------------------------------------------------
-function naiveSearch(long, short) {
-  var count = 0;
-  for (var i = 0; i < long.length; i++) {
-    for (var j = 0; j < short.length; j++) {
-      if (short[j] !== long[i + j]) break;
-      if (j === short.length - 1) count++;
-    }
-  }
-  return count;
-}
-naiveSearch("lorie loled", "lol")
-
-//-----------------------------------
-// sorting algorithm
 // bubble sort
 function bubbleSort(arr) {
   for (var i = arr.length; i > 0; i--) {
@@ -233,7 +44,9 @@ function bubbleSort(arr) {
 
 bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
 // LEGACY VERSION (non ES2015 syntax)
-function sselectionSort(arr) {
+// =================================
+
+function selectionSort(arr) {
   for (var i = 0; i < arr.length; i++) {
     var lowest = i;
     for (var j = i + 1; j < arr.length; j++) {
@@ -246,6 +59,24 @@ function sselectionSort(arr) {
       var temp = arr[i];
       arr[i] = arr[lowest];
       arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+//---------------------------
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+      //swap()
     }
   }
   return arr;
@@ -270,8 +101,20 @@ function selectionSort(arr) {
 }
 
 selectionSort([0, 2, 34, 22, 10, 19, 17]);
+//-------------------------------
+function naiveSearch(long, short) {
+  var count = 0;
+  for (var i = 0; i < long.length; i++) {
+    for (var j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+naiveSearch("lorie loled", "lol")
 
-//insertion sort
+// ========================
 function insertionSort(arr) {
   var currentVal;
   for (var i = 1; i < arr.length; i++) {
@@ -284,8 +127,56 @@ function insertionSort(arr) {
   return arr;
 }
 
+function insertionSort(arr) {
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[0]) {
+      //move current element to the first position
+      arr.unshift(arr.splice(i, 1)[0]);
+    } else if (arr[i] > arr[i - 1]) {
+      //leave current element where it is
+      continue;
+    } else {
+      //find where element should go
+      for (var j = 1; j < i; j++) {
+        if (arr[i] > arr[j - 1] && arr[i] < arr[j]) {
+          //move element
+          arr.splice(j, 0, arr.splice(i, 1)[0]);
+        }
+      }
+    }
+  }
+  return arr;
+}
+
 insertionSort([2, 1, 9, 76, 4])
 // Merges two already sorted arrays
+
+// =======================================================
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
+}
+//////////////////////------------------------
 function merge(arr1, arr2) {
   let results = [];
   let i = 0;
@@ -322,61 +213,12 @@ function mergeSort(arr) {
 }
 mergeSort([10, 24, 76, 73])
 //---------------------------------------------------------------
+// ==========================================================
 // Version with ES2015 Syntax
-function pivot(arr, start = 0, end = arr.length - 1) {
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-  };
-
-  // We are assuming the pivot is always the first element
-  let pivot = arr[start];
-  let swapIdx = start;
-
-  for (let i = start + 1; i <= end; i++) {
-    if (pivot > arr[i]) {
-      swapIdx++;
-      swap(arr, swapIdx, i);
-    }
-  }
-
-  // Swap the pivot from the start the swapPoint
-  swap(arr, start, swapIdx);
-  return swapIdx;
-}
 pivot([4, 8, 2, 1, 5, 7, 6, 3])
 
-// First Version
-function pivot(arr, start = 0, end = arr.length - 1) {
-  function swap(array, i, j) {
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  var pivot = arr[start];
-  var swapIdx = start;
-  for (var i = start + 1; i < arr.length; i++) {
-    if (pivot > arr[i]) {
-      swapIdx++;
-      swap(arr, swapIdx, i);
-    }
-  }
-  swap(arr, start, swapIdx);
-  return swapIdx;
-}
 
-function quickSort(arr, left = 0, right = arr.length - 1) {
-  if (left < right) {
-    let pivotIndex = pivot(arr, left, right) //3
-    //left
-    quickSort(arr, left, pivotIndex - 1);
-    //right
-    quickSort(arr, pivotIndex + 1, right);
-  }
-  return arr;
-}
-
-
-
+// 1 First Version-----------------------------------------
 function quick_Sort(arr) {
   if (arr.length <= 1) {
     return arr;
@@ -399,6 +241,153 @@ function quick_Sort(arr) {
     return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
   }
 }
+
+// 2-----------------------------
+function pivot(arr, start = 0, end = arr.length - 1) {
+  function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  var pivot = arr[start];
+  var swapIdx = start;
+  for (var i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right) //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+//3----------------------------------------
+function swap(array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
+
+function partition(array, pivot, left, right) {
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function quickSort(array, left, right) {
+  const len = array.length;
+  let pivot;
+  let partitionIndex;
+
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+
+    //sort left and right
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
+
+//////////////////////////////////////////////
+// //#1 - Sort 10 schools around your house by distance:
+// insertion sort
+
+// //#2 - eBay sorts listings by the current Bid amount:
+// radix or counting sort
+
+// //#3 - Sort scores on ESPN
+// Quick sort
+
+// //#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// Merge Sort
+
+// //#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// Insertion Sort
+
+// //#6 - Temperature Records for the past 50 years in Canada
+// radix or counting Sort
+// Quick sort if decimal places
+
+// //#7 - Large user name database needs to be sorted. Data is very random.
+// Quick sort
+
+// //#8 - You want to teach sorting
+// Bubble sort
+//============================
+//binary search 
+// Original Solution
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === elem) {
+    return middle;
+  }
+  return -1;
+}
+
+// Refactored Version
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103)
+
 //--------------------------------------
 // radix sort
 function getDigit(num, i) {
