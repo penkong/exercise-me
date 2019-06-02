ead864f7c640ccff7b37d7b6d87a38e3c30b29fc
mazdak <nazemi.works@gmail.com>
2019-06-01 20:58:01 -0700

    refactor

--- 3.sorters.js
+++ 3.sorters.js
@@ -116,12 +116,9 @@ naiveSearch("lorie loled", "lol")
 
 // ========================
 function insertionSort(arr) {
-  const len = arr.length;
-  for (let i = 0; i < len; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let el = arr[i];
-    let j;
-
-    for (j = i - 1; j >= 0 && arr[j] > el; j--) {
+    for (let j = i - 1; j >= 0 && arr[j] > el; j--) {
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = el;
